0cb9920e7a8b40901f3eef7925006cb07c35a1b3
Ali Bahşişoğlu <ali.bahsisoglu@barcin.com>
2015-11-30 00:14:51 +0200

    barcin line chart view deneme

---
+++ Charts/Classes/BarcinLineChartView.swift
@@ -0,0 +1,19 @@
+//
+//  BarcinLineChartView.swift
+//  Charts
+//
+//  Created by Ali Bahşişoğlu on 30/11/15.
+//  Copyright © 2015 dcg. All rights reserved.
+//
+
+import Foundation
+
+
+public class BarcinLineChartView : LineChartView {
+    internal override func initialize()
+    {
+        super.initialize()
+        
+        renderer = LineChartRenderer(dataProvider: self, animator: _animator, viewPortHandler: _viewPortHandler)
+    }
+}
